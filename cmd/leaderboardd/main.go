28033a5233298412656138726b74d124354eb7e0
Developer Experience team at Ignite <hello@ignite.com>
2023-02-04 12:12:42 -0800

    Initialized with Ignite CLI

---
+++ cmd/leaderboardd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/RaspiRepo/leaderboard/app"
+	"github.com/RaspiRepo/leaderboard/cmd/leaderboardd/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
+	}
+}
